7ff3976c28e7b9253d037fd5f141ddab46c60c23
merlin <romain.dary@eoko.fr>
2015-08-28 17:16:03 +0200

    add type and value oon field

--- src/Eoko/ODM/Metadata/Annotation/AbstractField.php
+++ src/Eoko/ODM/Metadata/Annotation/AbstractField.php
@@ -3,11 +3,30 @@
 namespace Eoko\ODM\Metadata\Annotation;
 
 use Doctrine\Common\Annotations\Annotation;
+use Eoko\ODM\DocumentManager\Metadata\FieldInterface;
 
-abstract class AbstractField extends Annotation
+abstract class AbstractField extends Annotation implements FieldInterface
 {
     public $name;
     public $type = 'string';
     public $nullable = false;
     public $options = [];
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+
 }
